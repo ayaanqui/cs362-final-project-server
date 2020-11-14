c8b5c7995be58ea315b3b0884806e19e95be9f6e
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-11-13 22:54:12 -0600

    JSON response for '/' path

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3200;
 
-app.get('/', (_, res) => res.send('CS362 Plant light and temperature measurer server...'));
+app.get('/', (_, res) => {
+  res.send({
+    message: 'Welcome to the Plant light and temperature measurer API'
+  });
+});
 app.use((_, res) => res.status(404).send({ message: '404, invalid endpoint' }));
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
