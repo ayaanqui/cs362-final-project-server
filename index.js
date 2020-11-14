967eeac681b7b731eb24eb088964fd47ebe71a07
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-11-13 20:42:08 -0600

    Added 404 endpoint

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3200;
 
-app.get('/', (req, res) => res.send('CS362 Plant light and temperature measurer server...'));
+app.get('/', (_, res) => res.send('CS362 Plant light and temperature measurer server...'));
+app.use((_, res) => res.status(404).send({ message: '404, invalid endpoint' }));
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
