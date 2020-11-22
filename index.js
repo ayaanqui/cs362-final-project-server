c68c7e29e556073cad8d013da2705b396fb7e346
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-11-21 20:41:09 -0600

    Temperature, humidity, and light query parameters

--- index.js
+++ index.js
@@ -6,8 +6,48 @@ const port = 3200;
 // Use cors
 app.use(cors());
 
-app.get('/', cors(), (_, res) => {
-  res.send({
+const validateTemp = temp => {
+  if (temp > 80)
+    return "HOT";
+  else if (temp < 20)
+    return "COLD";
+  else
+    return "GOOD";
+};
+
+const validateHum = hum => {
+  if (hum > 500)
+    return "HIGH";
+  else if (hum < 100)
+    return "LOW";
+  else
+    return "GOOD";
+};
+
+const validateLight = light => {
+  if (light > 500)
+    return "BRIGHT";
+  else if (light < 100)
+    return "DIM";
+  else
+    return "GOOD";
+};
+
+app.get('/', cors(), (req, res) => {
+  const temp = parseFloat(req.query.temp);
+  const hum = parseFloat(req.query.hum);
+  const light = parseFloat(req.query.light);
+
+  if (temp && hum && light) {
+    const ob = {
+      temp: validateTemp(temp),
+      hum: validateHum(hum),
+      light: validateLight(light),
+    };
+    return res.status(200).send(ob);
+  }
+
+  return res.send({
     message: 'Welcome to the Plant light and temperature measurer API'
   });
 });
